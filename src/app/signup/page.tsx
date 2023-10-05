0dd5aa31914a303fee874ec7cc44e9376cf67f3b
VikasMeshram2708 <meshramvikas744@gmail.com>
2023-10-05 21:43:25 +0530

    :sparkles: DONE: integrated signup api with front-end signup page.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 interface IUser {
   username: string;
@@ -11,21 +13,53 @@ interface IUser {
 }
 const SignUpPage = () => {
   const router = useRouter();
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [userData, setUserData] = useState<IUser>({
     username: "",
     email: "",
     password: "",
   });
-  async function onSignUp(event:any) {
-    event.preventDefault();
-    console.log(userData);
-    router.push("/login");
+  async function onSignUp(event: any) {
+    try {
+      event.preventDefault();
+      setLoading(true);
+
+      const response = await axios.post("/api/users/signup",userData);
+      console.log(response.data);
+
+
+      console.log(userData);
+      router.push("/login");
+    } catch (error) {
+      const errorMessage = error as Error;
+
+      return Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: errorMessage.message,
+      });
+    } finally {
+      setLoading(false);
+    }
   }
+
+  useEffect(() => {
+    if (
+      userData.email.length > 0 &&
+      userData.password.length > 0 &&
+      userData.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [userData]); // how often do we want to run this useEffect
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#1A1C26]">
       <div className="max-w-md w-full p-6 bg-[#202330] rounded-lg shadow-lg">
         <h1 className="text-3xl font-semibold text-center mb-6 text-white">
-          Sign Up
+          {loading ? "Processing..." : "Sign Up"}
         </h1>
         <form onSubmit={onSignUp}>
           <div className="mb-4">
@@ -92,7 +126,7 @@ const SignUpPage = () => {
             type="submit"
             className="w-full bg-[#F41945] text-white py-2 rounded-lg hover:bg-pink-600 focus:outline-none focus:bg-pink-600"
           >
-            Sign Up
+            {buttonDisabled ? "No Sign Up" : "Sign Up"}
           </button>
           <Link href={"/login"}>
             <p className="text-sm font-semibold p-1">Already a User?</p>
