e3217a0bdf078e9ca980f8b57b6e1a7118767859
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-02-18 12:00:29 +0530

    :package: updated the navbar.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,40 +1,77 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect } from "react";
 
 export default function Navbar() {
   const { status, data } = useSession();
   useEffect(() => {
-    console.log('user-data', data)
-  },[data])
+    console.log("user-data", data);
+  }, [data]);
   return (
-    <nav>
-      <div className="flex sm:justify-between gap-5 flex-wrap rounded mt-5 p-3 items-center outline max-w-[90%] mx-auto">
-        <h1 className="text-3xl font-bold">
-          <Link href="/">iBook</Link>
-        </h1>
-        {status === "authenticated" ? (
-          <button
-            onClick={() => {
-              signOut();
-            }}
-            type="button"
-            className="text-lg btn btn-error btn-lg"
-          >
-            Logout
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              signIn();
-            }}
-            type="button"
-            className="text-lg btn btn-success btn-lg"
-          >
-            Sign In
-          </button>
+    <nav className="navbar shadow-lg bg-base-100">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-xl">
+          Vikas MeshRAM
+        </Link>
+      </div>
+      <div className="flex-none gap-2">
+        {/* <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div> */}
+        <button
+          onClick={() => signIn("google")}
+          type="button"
+          className="btn btn-lg btn-accent"
+        >
+          {status === "loading" ? "Loading..." : "Sign In"}
+        </button>
+        {status === "authenticated" && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  alt={data.user?.name!}
+                  src={data.user?.image!}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <button
+                  onClick={() => signOut()}
+                  type="button"
+                  className="btn btn-error btn-sm"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
         )}
       </div>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,28 @@ export default function Home() {
   const { data } = useSession();
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4">{data?.user?.name}</h1>
-        <div className="flex items-center">
-          <div className="mr-4">
-            <div className="w-24 h-24 rounded-full overflow-hidden">
-              <Image
-                width={500}
-                height={500}
-                alt={data?.user?.name!}
-                src={data?.user?.image!}
-              />
+      {data && (
+        <div className="bg-white p-8 rounded-lg shadow-md">
+          <h1 className="text-3xl font-bold mb-4">{data?.user?.name}</h1>
+          <div className="flex items-center">
+            <div className="mr-4">
+              <div className="w-24 h-24 rounded-full overflow-hidden">
+                <Image
+                  width={500}
+                  height={500}
+                  alt={data?.user?.name!}
+                  src={data?.user?.image!}
+                />
+              </div>
+            </div>
+            <div>
+              <p className="italic">{data?.user?.email}</p>
             </div>
           </div>
-          <div>
-            <p className="italic">{data?.user?.email}</p>
-          </div>
         </div>
-      </div>
+      )}
+
+      {!data && <p className="text-3xl font-semibold">Please Login First!</p>}
     </main>
   );
 }
